8b864e51859d622568aa6f3ec97ae1891f63fea5
Shawn Caeiro <shawncaeiro@gmail.com>
2015-07-09 23:44:25 -0500

    Initial exploration of three main parts to application.

---
+++ exploratory/elasticsearch/elasticsearch.py
@@ -0,0 +1,34 @@
+# ES Testing
+from elasticsearch import Elasticsearch 
+
+BONSAI_URL = "INSERT_URL"
+
+# Creating elasticsearch and index
+es = Elasticsearch(BONSAI_URL)
+es.indices.create(index='news', ignore=400)
+
+# Adding and retrieving a document
+doc = {
+    'domain': 'CNN',
+    'date': datetime(2010, 10, 10, 10, 10, 10),
+    'text': 'This is an article.'
+}
+res = es.index(index="news", doc_type='article', id=1, body=doc)
+res = es.get(index="news", doc_type='article', id=1)
+es.indices.refresh(index="news")
+
+# Returning all documents
+res = es.search(index="news", body={"query": {"match_all": {}}})
+print("Got %d Hits:" % res['hits']['total'])
+for hit in res['hits']['hits']:
+    print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
+
+# Finding simlarity
+# NOTE similarity does not work for test sentences. 
+# A senstence will generally not be simialr enough regardless
+es.mlt(index='news', doc_type="article",
+                          id=doc_id, mlt_fields="text", 
+                          search_size=7,
+                          min_term_freq=0,
+                          min_doc_freq=0,
+                          percent_terms_to_match= 0)
---
+++ exploratory/newspaper/newspaper.py
@@ -0,0 +1,66 @@
+# Newspaper Stuff
+
+import newspaper
+from newspaper import Article
+from collections import defaultdict
+import pickle
+import datetime, date, timedelta
+import pytz
+
+def buildNewspaper():
+	cnn_paper = newspaper.build(u'http://cnn.com', memoize_articles=False)
+	a = defaultdict(int)
+	print cnn_paper.size()
+	for index, art in enumerate(cnn_paper2.articles):
+		url = art.url
+		article = Article(url)
+		article.download()
+		article.parse()
+		if article.publish_date:
+			a[url] = article.publish_date
+		print "Article Added " + str(index) 
+	pickle.dump( a, open( "unsorted.p", "wb" ) )
+
+def sortNewspaper():
+	utc = pytz.UTC
+	toDelete = []
+
+	news = pickle.load( open( "unsorted.p", "rb" ) )
+	for i, key in enumerate(news.keys()):
+		try:
+			news[key] = utc.localize(news[key]) 
+		except:
+			toDelete.append(key)
+		print "yay " + str(i)
+	print toDelete
+	for k in toDelete:
+		news.pop(k, None)
+	sortedNews = sorted(news.items(), key=lambda p: p[1])
+	print sortedNews
+	pickle.dump( sortedNews, open( "sorted.p", "wb" ) )
+
+def dayCount():
+	news = pickle.load( open( "sorted.p", "rb" ) )
+	for days_to_subtract in range(0,30):	
+		total = 0
+		d = (utc.localize(datetime.today() - timedelta(days=days_to_subtract) ).date()
+		for i, key in enumerate(news.keys()):
+			if news[key].date() == d:
+				total +=1	
+		print "{}\tdocuments from\t{}".format(total,d)
+
+def newsToday():
+	for i, key in enumerate(news.keys()):
+		if news[key].date() == utc.localize(datetime.today() - timedelta(days=0) ).date():
+			print key
+
+
+
+
+
+
+
+
+
+
+
